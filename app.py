dac2d1fdb25e870604cf7b3a40dab8d59e9532be
themaliboo <azacazacazggvp@gmail.com>
2026-04-07 23:03:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1931,4 +1931,6 @@ ERROR_TEMPLATE = '''
 '''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
